8f0d68e6553f9733dea8ae507f4c0d9db08e5d8c
Dhiraj Kumar <kumardhiraj7643@gmail.com>
2026-04-18 03:16:09 +0530

    Fix code and logic

--- sum_of_natural_no._for_loop.cpp
+++ sum_of_natural_no._for_loop.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
     int n, sum = 0;
 
-    cout << "Enter a number: ";
+    cout << "Enter a number:";
     cin >> n;
 
     for(int i = 1; i <= n; i++) {
         sum =sum+i;
     }
 
-    cout << "Sum = " << sum;
+    cout << "Sum = "<< sum;
 
     return 0;
 }
