13838a50957e1eb0d064ecc54cc2e42b69822c56
Dhiraj Kumar <kumardhiraj7643@gmail.com>
2026-04-18 02:37:43 +0530

    Removed exe files and added gitignore

---
+++ reverse_of_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, rev = 0;
+    cout<<"enter a number:-";
+    cin >> n;
+
+    while (n != 0) {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+
+    cout << "Reverse: " << rev;
+
+    return 0;
+}
